3eacb94c16fac71c991cdd1c7abe161e24ce0728
Jeremy Panjaitan <jeremypanjaitan@gmail.com>
2022-03-24 20:29:44 +0700

    [TEST] create new transaction method

--- dev/test.js
+++ dev/test.js
@@ -2,7 +2,15 @@ const Blockchain = require("./blockchain");
 
 const bitcoin = new Blockchain();
 
-bitcoin.createNewBlock(2389, "AFSDLKFJASLDKFJ", "AJFLAKSJDFLKASDLF");
-bitcoin.createNewBlock(2388, "AFSDLKFJASLDKFJ", "AJFLAKSJDFLKASDLF");
-bitcoin.createNewBlock(2387, "AFSDLKFJASLDKFJ", "AJFLAKSJDFLKASDLF");
-console.log(bitcoin);
+bitcoin.createNewBlock(4321, "ASDF329814723ASDF", "ASDLKFJASDLKFASD");
+
+bitcoin.createNewTransaction(100, "ALEXFAJSDKLFAJSDF", "JENN9JSDLKFASJDF");
+
+bitcoin.createNewBlock(429384, "ASDFASLDKFJASLKD98234", "ALSDKFJALSDKFASD");
+
+bitcoin.createNewTransaction(50, "ALEXFAJSDKLFAJSDF", "JENN9JSDLKFASJDF");
+bitcoin.createNewTransaction(300, "ALEXFAJSDKLFAJSDF", "JENN9JSDLKFASJDF");
+bitcoin.createNewTransaction(2000, "ALEXFAJSDKLFAJSDF", "JENN9JSDLKFASJDF");
+
+bitcoin.createNewBlock(982374, "EOWIRUQWEASDLKF", "QWEORWEFFASDLKFJAS");
+console.dir(bitcoin, { depth: null });
