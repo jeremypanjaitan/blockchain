0ad358cc5c37f2e90f9d1fe4c822da9c8ce33a9f
Jeremy Panjaitan <jeremypanjaitan@gmail.com>
2022-03-26 18:47:39 +0700

    [TEST] hash block method

--- dev/test.js
+++ dev/test.js
@@ -2,15 +2,24 @@ const Blockchain = require("./blockchain");
 
 const bitcoin = new Blockchain();
 
-bitcoin.createNewBlock(4321, "ASDF329814723ASDF", "ASDLKFJASDLKFASD");
+const previousBlockHash = "ALDKJFASDFALSKDFJALSKDF";
+const currentbBlockData = [
+  {
+    amount: 10,
+    sender: "ASDLFKASDLFKASDF",
+    recipient: "ASALDKFJASDFLAKSD",
+  },
+  {
+    amount: 30,
+    sender: "CADSFASDZCVZCVLKJJKJ",
+    recipient: "CVZXKVLZXKCVLKZXCVK",
+  },
+  {
+    amount: 200,
+    sender: "R13432ASDLFJSADFK",
+    recipient: "POIUOIUPOIASDKFJ",
+  },
+];
+const nonce = 100;
 
-bitcoin.createNewTransaction(100, "ALEXFAJSDKLFAJSDF", "JENN9JSDLKFASJDF");
-
-bitcoin.createNewBlock(429384, "ASDFASLDKFJASLKD98234", "ALSDKFJALSDKFASD");
-
-bitcoin.createNewTransaction(50, "ALEXFAJSDKLFAJSDF", "JENN9JSDLKFASJDF");
-bitcoin.createNewTransaction(300, "ALEXFAJSDKLFAJSDF", "JENN9JSDLKFASJDF");
-bitcoin.createNewTransaction(2000, "ALEXFAJSDKLFAJSDF", "JENN9JSDLKFASJDF");
-
-bitcoin.createNewBlock(982374, "EOWIRUQWEASDLKF", "QWEORWEFFASDLKFJAS");
-console.dir(bitcoin, { depth: null });
+console.log(bitcoin.hashBlock(previousBlockHash, currentbBlockData, nonce));
