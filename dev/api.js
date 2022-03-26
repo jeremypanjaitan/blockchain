091e9cfe6e4ae5c84e3836d95290d37bf644fdec
Jeremy Panjaitan <jeremypanjaitan@gmail.com>
2022-03-26 20:44:39 +0700

    [FEATURE] get blockchain route

---
+++ dev/api.js
@@ -0,0 +1,24 @@
+var express = require("express");
+var app = express();
+const bodyParser = require("body-parser");
+const Blockchain = require("./blockchain");
+
+const bitcoin = new Blockchain();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get("/blockchain", (req, res) => {
+  res.send(bitcoin);
+});
+
+app.post("/transaction", (req, res) => {
+  console.log(req.body);
+  res.send(`The amount of the transaction is ${req.body.amount} bitcoin.`);
+});
+
+app.get("/mine", (req, res) => {});
+
+app.listen(3000, () => {
+  console.log("Listening on port 3000...");
+});
