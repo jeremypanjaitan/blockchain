ce315b72887fca2f8f590d572ca6695b2edcd964
Jeremy Panjaitan <jeremypanjaitan@gmail.com>
2022-03-26 20:15:42 +0700

    [FEATURE] creating a genesis block

--- dev/test.js
+++ dev/test.js
@@ -2,24 +2,4 @@ const Blockchain = require("./blockchain");
 
 const bitcoin = new Blockchain();
 
-const previousBlockHash = "ALDKJFASDFALSKDFJALSKDF";
-const currentbBlockData = [
-  {
-    amount: 10,
-    sender: "ASDLFKASDLFKASDF",
-    recipient: "ASALDKFJASDFLAKSD",
-  },
-  {
-    amount: 30,
-    sender: "CADSFASDZCVZCVLKJJKJ",
-    recipient: "CVZXKVLZXKCVLKZXCVK",
-  },
-  {
-    amount: 200,
-    sender: "R13432ASDLFJSADFK",
-    recipient: "POIUOIUPOIASDKFJ",
-  },
-];
-
-console.log(bitcoin.proofOfWork(previousBlockHash, currentbBlockData));
-console.log(bitcoin.hashBlock(previousBlockHash, currentbBlockData, 136058));
+console.log(bitcoin);
